468ee52e4d857d7f4d65ec6528f5af2306396a1d
Matheus <matheusmoraes.65@gmail.com>
2023-05-08 22:18:29 -0300

    Adicionando banco de dados mais funciomaneto

--- pjrAtiv/Form1.cs
+++ pjrAtiv/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace pjrAtiv
 {
     public partial class Form1 : Form
@@ -19,22 +21,37 @@ namespace pjrAtiv
 
         private void btnLogar_Click(object sender, EventArgs e)
         {
-            if (txtLogin.Text == "12345678901")
+            string mensagem;
+
+            if (txtLogin.Text == String.Empty || txtLogin.TextLength<11)
             {
-                lblMensagem.Text = "Usuário autenticado!";
-                if (txtSenha.Text== "123456")
+                mensagem = "Prencha a caixa de login corretamente!";
+            }
+            else if (txtSenha.Text == String.Empty || txtSenha.TextLength<6)
+            {
+                mensagem= "Prencha a caixa de senha corretamente!";
+            }
+            else
+            {
+                if (txtLogin.Text != "12345678901")
                 {
-                    lblMensagem.Text = " Usuário Auntenticado";
+                    mensagem = "Login inválido!";
                 }
+                else if (txtSenha.Text != "123456")
+                {
+                    mensagem = "Senha inválida";
+                }
+                else
+                {
+                    mensagem = "Usuário autenticado!";
+                }
+            }
 
-                
 
-
-
-            
-           }
-            else lblMensagem.Text = "Usuário não encontrado";
+            //lblMensagem.Text = mensagem;  // mensagem em um label
+            MessageBox.Show(mensagem, "Aviso!",MessageBoxButtons.OK,MessageBoxIcon.Warning);
         }
+            
         private void lblSenha_Click(object sender, EventArgs e)
         {
 
